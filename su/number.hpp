725cefa793f92b0b38a2b3f6c96583ba0bf218cf
edmondj <julien.edmond@epitech.eu>
2018-03-26 08:57:55 -0400

    Added conversion concept and started testing

--- su/number.hpp
+++ su/number.hpp
@@ -21,6 +21,25 @@ constexpr auto apply_ratio(const T& value)
   return value * Ratio::num / Ratio::den;
 }
 
+template<typename SrcUnit, typename DestUnit>
+struct converter {};
+
+template<typename Unit>
+struct converter<Unit, Unit>
+{
+  template<typename T>
+  static constexpr T convert(const T& value) { return value; }
+};
+
+template<typename T, typename SrcUnit, typename DestUnit, typename = void>
+struct is_convertible : public std::false_type {};
+
+template<typename T, typename SrcUnit, typename DestUnit>
+struct is_convertible<T, SrcUnit, DestUnit, std::void_t<decltype(converter<SrcUnit, DestUnit>::template convert<T>(std::declval<T>()))>> : public std::true_type {};
+
+template<typename T, typename SrcUnit, typename DestUnit>
+constexpr bool is_convertible_v = is_convertible<T, SrcUnit, DestUnit>::value;
+
 template<typename T, typename Ratio, typename Unit>
 class number;
 
@@ -42,15 +61,17 @@ class number
   static_assert(is_ratio_v<Ratio>, "Ratio must be of type std::ratio");
 
 public:
+  using value_type = T;
+  using ratio = Ratio;
+  using unit = Unit;
+  using _type = number<T, Ratio, Unit>;
 
-  using type = number<T, Ratio, Unit>;
-
-  template<typename U, typename = std::enable_if_t<std::is_constructible_v<T, U> && !is_unitless_v<type>>>
+  template<typename U, typename = std::enable_if_t<std::is_constructible_v<T, U> && !is_unitless_v<_type>>>
   constexpr explicit number(U&& value)
     : _value(std::forward<U>(value))
   {}
 
-  template<typename U, typename = std::enable_if_t<std::is_constructible_v<T, U> && is_unitless_v<type>>, typename = void>
+  template<typename U, typename = std::enable_if_t<std::is_constructible_v<T, U> && is_unitless_v<_type>>, typename = void>
   constexpr number(U&& value)
     : _value(std::forward<U>(value))
   {}
@@ -60,21 +81,23 @@ public:
   number(number&& other) = default;
   number& operator=(number&& other) = default;
 
+  template<typename U, typename = std::enable_if_t<std::is_assignable_v<T&, U>>>
+  number& operator=(U&& value)
+  {
+    _value = std::forward<U>(value);
+    return *this;
+  }
+
   constexpr T& value() { return _value; }
   constexpr const T& value() const { return _value; }
 
   constexpr explicit operator T&() { return value(); }
   constexpr explicit operator const T&() const { return value(); }
 
-  template<typename U, typename DestRatio, typename DestUnit>
+  template<typename U, typename DestRatio, typename DestUnit, typename = std::enable_if_t<std::is_constructible_v<U, T> && is_convertible_v<T, Unit, DestUnit>>>
   constexpr operator number<U, DestRatio, DestUnit>()
   {
-    constexpr bool same_unit = std::is_same_v<Unit, DestUnit>;
-
-    static_assert(same_unit, "No known conversion");
-
-    if constexpr (same_unit)
-      return number<U, DestRatio, DestUnit>(apply_ratio<T, std::ratio_divide<Ratio, DestRatio>>(_value));
+      return number<U, DestRatio, DestUnit>(apply_ratio<T, std::ratio_divide<Ratio, DestRatio>>(converter<Unit, DestUnit>::template convert<T>(_value)));
   }
 
 private:
--- tests/number_test.cpp
+++ tests/number_test.cpp
@@ -1,15 +1,45 @@
 #include "number.hpp"
 #include "catch.hpp"
 
-using numberType1 = number<double, std::ratio<1>, struct numberType1Unit>;
+using number_type_1 = number<double, std::ratio<1>, struct numberType1Unit>;
 
-//static_assert(std::is_constructible_v<numberType1, double>);
-//static_assert(!std::is_invocable_v<void(*)(numberType1), double>);
-//
-//static_assert(std::is_constructible_v<unitless<double>, double>);
-//static_assert(std::is_invocable_v<void(*)(unitless<double>), double>);
+static_assert(std::is_constructible_v<number_type_1, double>);
+static_assert(!std::is_invocable_v<void(*)(number_type_1), double>);
 
-void foo()
+static_assert(std::is_constructible_v<unitless<double>, double>);
+static_assert(std::is_invocable_v<void(*)(unitless<double>), double>);
+
+template<typename Ratio = std::ratio<1>>
+using first_number = number<double, Ratio, struct first_unit>;
+
+
+template<typename Ratio = std::ratio<1>>
+using second_number = number<double, Ratio, struct second_unit>;
+
+template<>
+struct converter<first_number<>::unit, second_number<>::unit>
 {
-  unitless<double> uless((double)(3.0));
+  template<typename T>
+  constexpr static T convert(const T& value) { return value * 13; }
+};
+
+template<>
+struct converter<second_number<>::unit, first_number<>::unit>
+{
+  template<typename T>
+  constexpr static T convert(const T& value) { return value / 13; }
+};
+
+TEST_CASE("Same unit ratio conversion")
+{
+  first_number<std::ratio<1>> unit(3.0);
+  first_number<std::kilo> kilo = unit;
+  REQUIRE(kilo.value() == 3e-3);
+
+  first_number<std::milli> milli = kilo;
+  REQUIRE(milli.value() == 3e3);
+
+  milli = 13;
+  unit = milli;
+  REQUIRE(unit.value() == 13e-3);
 }
