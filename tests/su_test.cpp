52d0c89cf1cc9e81fe380ed5cdc6feda8db6274e
edmondj <julien.edmond@epitech.eu>
2018-04-21 15:09:30 -0400

    Added utf-8 support to gcc and clang (hopefully)

--- tests/su_test.cpp
+++ tests/su_test.cpp
@@ -10,7 +10,7 @@ using namespace su::literals;
 TEST_CASE("Length conversions")
 {
   REQUIRE(su::meter<double>(su::inch<double>(42)).value() == 1.0668);
-  REQUIRE(su::inch<long double>(1.0668_m).value() == 42);
+  REQUIRE(su::inch<long double>(1.0668_m).value() == Approx(42));
   REQUIRE(su::meter<double>(su::foot<double>(42)).value() == Approx(12.8016));
   REQUIRE(su::foot<long double>(12.8016_m).value() == Approx(42));
   REQUIRE(su::meter<double>(su::yard<double>(42)).value() == Approx(38.4048));
