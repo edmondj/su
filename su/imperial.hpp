af5dcee228f4a68f75a91f120639686d9cb70362
edmondj <julien.edmond@epitech.eu>
2018-04-22 09:12:18 -0400

    Added more imperial units of length and their literals

--- su/imperial.hpp
+++ su/imperial.hpp
@@ -14,6 +14,7 @@ namespace su
   };
 
   SU_BASIC_CONVERTER(inch_unit, meter_unit, 0.0254)
+  SU_DECLARE_LITERAL(inch, in)
 
 
   SU_DECLARE_UNIT(foot)
@@ -26,6 +27,7 @@ namespace su
   };
 
   SU_BASIC_CONVERTER(foot_unit, inch_unit, 12.0)
+  SU_DECLARE_LITERAL(foot, ft)
 
 
   SU_DECLARE_UNIT(yard)
@@ -38,5 +40,18 @@ namespace su
   };
 
   SU_BASIC_CONVERTER(yard_unit, foot_unit, 3)
+  SU_DECLARE_LITERAL(yard, yd)
 
+  
+  SU_DECLARE_UNIT(mile)
+
+  template<>
+  struct unit_metadata<mile_unit>
+  {
+    constexpr static const char* symbol = "mi";
+    using fallback_unit = yard_unit;
+  };
+
+  SU_BASIC_CONVERTER(mile_unit, yard_unit, 1760)
+  SU_DECLARE_LITERAL(mile, mi)
 }
--- tests/su_test.cpp
+++ tests/su_test.cpp
@@ -9,10 +9,15 @@ using namespace su::literals;
 
 TEST_CASE("Length conversions")
 {
-  REQUIRE(su::meter<double>(su::inch<double>(42)).value() == 1.0668);
+  REQUIRE(su::meter<long double>(42_in).value() == 1.0668);
   REQUIRE(su::inch<long double>(1.0668_m).value() == Approx(42));
-  REQUIRE(su::meter<double>(su::foot<double>(42)).value() == Approx(12.8016));
+  
+  REQUIRE(su::meter<long double>(42_ft).value() == Approx(12.8016));
   REQUIRE(su::foot<long double>(12.8016_m).value() == Approx(42));
-  REQUIRE(su::meter<double>(su::yard<double>(42)).value() == Approx(38.4048));
+  
+  REQUIRE(su::meter<long double>(42_yd).value() == Approx(38.4048));
   REQUIRE(su::yard<long double>(38.4048_m).value() == Approx(42));
+
+  REQUIRE(su::kilometer<long double>(42_mi).value() == Approx(67.59245));
+  REQUIRE(su::mile<long double>(67.59245_km).value() == Approx(42));
 }
